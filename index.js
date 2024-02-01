0e715e9ac2545cdad7d1ddb4638880caa3786ee3
ChaseHarding <chaseharding965@gmail.com>
2024-01-31 21:17:19 -0800

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 // the value stored inexpress is actually a function, so now we call it to assign it to a variable 
@@ -10,7 +11,10 @@ app.get('/second', (req, res) => {
     res.send('Hello to my Second Route');
 })
 
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('Server running on port 3000...')
 });
