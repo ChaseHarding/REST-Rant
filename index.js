4bdcc4923875169f8fe89d7db711add2c84e6913
ChaseHarding <chaseharding965@gmail.com>
2024-02-04 17:48:25 -0800

    Adding home page view

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const express = require('express');
 const app = express();
 // the value stored inexpress is actually a function, so now we call it to assign it to a variable 
 
-app.use('places', require('./controllers/places'))
+// adding code above the routes to define the view engine(JSX)
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+app.use('/places', require('./controllers/places'))
 
 app.get ('/', (req, res) => {
-    res.send('Hello World')
+    res.render('home')
 })
 
 app.get('/second', (req, res) => {
