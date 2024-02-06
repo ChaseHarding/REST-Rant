270d6466ee3e2d7cde3f75c2df2644e44c2fc5aa
ChaseHarding <chaseharding965@gmail.com>
2024-02-05 21:52:05 -0800

    Adding Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -2,17 +2,17 @@ const router = require('express').Router()
 
 router.get('/', (req, res) => {
     let places = [{
-        name: 'H-Thai-ML',
+        name: 'Mels Drive-In',
         city: 'Seattle',
         state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        cuisines: 'Milkshakes, Burgers',
+        pic: '/images/melsDriveIn.jpg'
       }, {
-        name: 'Coding Cat Cafe',
+        name: 'McDonalds',
         city: 'Phoenix',
         state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        cuisines: 'Coffee, Burgers, Breakfast',
+        pic: '/images/mcDonalds.jpg'
       }]
       
     res.render('places/index', { places })
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home() {
     <Def>
       <main>
         <h1>HOME</h1>
+        <div>
+            <img src="/images/breadHomePage.jpg" alt="delicious bread" />
+            <div>
+            Photo by <a href="https://unsplash.com/@wesual?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Wesual Click</a> on <a href="https://unsplash.com/photos/cereal-and-three-buns-rsWZ-P9FbQ4?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+            </div>
+        </div>
         <a href="/places">
           <button className="btn btn-primary">Places Page</button>
         </a>
