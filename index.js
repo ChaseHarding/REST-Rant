90cbae31ab83b00e7929822e543b45ce532e279a
ChaseHarding <chaseharding965@gmail.com>
2024-02-05 20:53:36 -0800

    Adding styling to REST-Rant

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express();
 // adding code above the routes to define the view engine(JSX)
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
 app.get ('/', (req, res) => {
@@ -21,6 +23,7 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
+// Listen for Connections
 app.listen(process.env.PORT, () => {
     console.log('Server running on port 3000...')
 });
